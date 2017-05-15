8c9c116a9fe9dcdaac7385d83f84a518ad4421eb
Andriy Zoryk <azoryk@gmail.com>
2017-05-15 16:28:41 +0200

    Update download_images.py

--- dataset/download_images.py
+++ dataset/download_images.py
@@ -3,16 +3,14 @@ import sys
 import shutil
 import urllib.request
 import os
+from datetime import datetime
 
 print(flickrapi.__version__)
 
 api_key = '285a24d486541ae9086afcfefd109f07' # flickr key
 api_secret = '24d0976e877a2a82' #flickr secret
 
-flickr = flickrapi.FlickrAPI(api_key, api_secret)
-photos = flickr.photos.search(user_id='94232412@N00', per_page='10')
-sets = flickr.photosets.getList(user_id='94232412@N00')
-
+flickr = flickrapi.FlickrAPI(api_key, api_secret, cache=True)
 '''
 This program finds the urls for the mentioned tag, e.g. 'Bamberger dom'
 '''
@@ -20,21 +18,27 @@ This program finds the urls for the mentioned tag, e.g. 'Bamberger dom'
 def get_url_for_tags(keyward):
    os.mkdir('image_tags_urls')
    count_url = 0
+   mintime = 1147651200
+   maxtime = 1300000000
+   print(datetime.fromtimestamp(mintime))
+   print(datetime.fromtimestamp(maxtime))
    print("tags to search: " )
    print(keyward)
    for tag in keyward:
        print(tag)
        print("Images by tag '" + tag + "' are searching...")
        photos = flickr.walk(api_key=api_key,
-                                        privacy_filter="1",
-                                        media="photos",
+                                        #privacy_filter="1",
+                                        #media="photos",
                                         per_page=500,
                                         tag_mode = 'all',
                                         tags = tag,
                                         text=tag,
-                                        accuracy="16",
+                                        #accuracy="16",
                                         extras = 'url_c',
-                                        sort="relevance"
+                                        #sort="relevance",
+                                        #min_taken_date='1147651200',
+                                        #max_taken_date='1400000000'
 
                          )
        file_path = tag + "_photos_url.txt"
@@ -64,21 +68,22 @@ def download_images(path):  # argument - path to the folder
 
     query_file = open(query_file_name, 'r', encoding='utf-8-sig')
     queris = []
-    print('reading query file')
+    print('Reading query file...')
     for line in query_file:
         queris = queris + [line[0:len(line) - 1]]
     url_list = []
-    print('tags to search: ')
+    print('Tags to search: ')
     print(queris)
     for file in queris:
+
         url_file = open(path +file + '_photos_url.txt', 'r', encoding='utf-8-sig')
-        print('reading "' + file + '_photos_url.txt"...' )
+        print('Reading "' + file + '_photos_url.txt"...' )
         for url1 in url_file:
             url_list = url_list + [url1[0:len(url1) - 1]]
         folder_path =  file + '_images'
-        print('creating folder "' + file + '_images"')
+        print('Creating folder "' + file + '_images"')
         os.mkdir(folder_path)
-        print('downloading "' + file  + '" images...')
+        print('Downloading "' + file  + '" images...')
         for url in url_list:
             name = url.split('/')[-1]
             f = open(folder_path + '/' + name, 'wb')
@@ -110,8 +115,8 @@ def main(*argv):
 
     get_url_for_tags(queris)
 
-    path = 'image_tags_urls/'
-    download_images(path)
+    #path = 'image_tags_urls/'
+    #download_images(path)
 
 if __name__ == '__main__':
     sys.exit(main(*sys.argv))
