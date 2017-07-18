bd38d6efa4de49c4deb92b04d2d01fe7d492429d
Andriy Zoryk <azoryk@gmail.com>
2017-07-18 14:26:15 +0200

    Create resnet50_event_model.py

---
+++ keras/working_models/resnet50_event_model.py
@@ -0,0 +1,541 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 10:13:14 2017
+source of model: 
+https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py
+@author: Administrator
+"""
+
+# -*- coding: utf-8 -*-
+#%%
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 10:13:14 2017
+source of model: 
+https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py
+@author: Administrator
+"""
+
+# -*- coding: utf-8 -*-
+#%%
+import os
+import numpy as np
+from scipy.misc import imread, imresize
+import matplotlib.pyplot as plt
+from keras.utils import np_utils
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+#from keras.models import Sequential
+from keras.optimizers import SGD
+#from keras.preprocessing import image
+#from keras.applications.imagenet_utils import decode_predictions
+#from keras.applications.imagenet_utils import preprocess_input
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Dropout, Flatten, merge, Reshape, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras import backend as K
+
+from sklearn.metrics import log_loss
+import matplotlib.pyplot as plt
+#from scipy.misc import toimage
+
+#from load_cifar10 import load_cifar10_data
+
+#%%
+
+def identity_block(input_tensor, kernel_size, filters, stage, block):
+    """
+    The identity_block is the block that has no conv layer at shortcut
+    Arguments
+        input_tensor: input tensor
+        kernel_size: defualt 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the nb_filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    """
+
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(nb_filter1, (1, 1), name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(nb_filter2, kernel_size, kernel_size,
+                      border_mode='same', name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(nb_filter3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    x = merge([x, input_tensor], mode='sum')
+    x = Activation('relu')(x)
+    return x
+
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+    """
+    conv_block is the block that has a conv layer at shortcut
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: defualt 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the nb_filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    Note that from stage 3, the first conv layer at main path is with subsample=(2,2)
+    And the shortcut should have subsample=(2,2) as well
+    """
+
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(nb_filter1, (1, 1), subsample=strides,
+                      name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(nb_filter2, kernel_size, kernel_size, border_mode='same',
+                      name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(nb_filter3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = Conv2D(nb_filter3, (1, 1), subsample=strides,
+                             name=conv_name_base + '1')(input_tensor)
+    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = merge([x, shortcut], mode='sum')
+    x = Activation('relu')(x)
+    return x
+
+def resnet50_model(img_rows, img_cols, color_type=1, num_classes=None):
+    """
+    Resnet 50 Model for Keras
+    Model Schema is based on 
+    https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py
+    ImageNet Pretrained Weights 
+    https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_th_dim_ordering_th_kernels.h5
+    Parameters:
+      img_rows, img_cols - resolution of inputs
+      channel - 1 for grayscale, 3 for color 
+      num_classes - number of class labels for our classification task
+    """
+
+    # Handle Dimension Ordering for different backends
+    global bn_axis
+    if K.image_dim_ordering() == 'tf':
+      bn_axis = 3
+      img_input = Input(shape=(img_rows, img_cols, color_type))
+    else:
+      bn_axis = 1
+      img_input = Input(shape=(color_type, img_rows, img_cols))
+
+    x = ZeroPadding2D((3, 3))(img_input)
+    x = Conv2D(64, 7, 7, subsample=(2, 2), name='conv1')(x)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+
+    # Fully Connected Softmax Layer
+    x_fc = AveragePooling2D((7, 7), name='avg_pool')(x)
+    x_fc = Flatten()(x_fc)
+    x_fc = Dense(1000, activation='softmax', name='fc1000')(x_fc)
+
+    # Create model
+    model = Model(img_input, x_fc)
+
+#    # Load ImageNet pre-trained data 
+#    if K.image_dim_ordering() == 'th':
+#      # Use pre-trained weights for Theano backend
+#      weights_path = 'imagenet_models/resnet50_weights_th_dim_ordering_th_kernels.h5'
+#    else:
+#      # Use pre-trained weights for Tensorflow backend
+    weights_path = r'C:\Users\Administrator\Desktop\az\resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+
+    model.load_weights(weights_path)
+
+    # Truncate and replace softmax layer for transfer learning
+    # Cannot use model.layers.pop() since model is not of Sequential() type
+    # The method below works since pre-trained weights are stored in layers but not in the model
+    x_newfc = AveragePooling2D((7, 7), name='avg_pool')(x)
+    x_newfc = Flatten()(x_newfc)
+    x_newfc = Dense(num_classes, activation='softmax', name='fc10')(x_newfc)
+
+    # Create another model with our customized softmax
+    model = Model(img_input, x_newfc)
+
+    # Learning rate is changed to 0.001
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+  
+    return model
+
+#%%
+
+if __name__ == '__main__':
+
+    # Example to fine-tune on 3000 samples from Cifar10
+
+    img_rows, img_cols = 224, 224 # Resolution of inputs
+    num_channel = 3
+    num_classes = 12 
+    batch_size = 16 
+    nb_epoch = 20
+    img_mode = 'RGB'
+
+    
+    # path of folder of images
+    data_path = r'C:\Users\Administrator\Desktop\az\poi_dataset_1k+\train'  
+    
+    # Define data path
+    
+    data_dir_list = os.listdir(data_path) #MacOS creates automatically '.DS_Store' file in each folder
+    if data_dir_list[0] == '.DS_Store':
+        data_dir_list = os.listdir(data_path)[1:]
+    print (data_dir_list)
+    
+    #list of all the images
+    img_data_list=[]
+
+#total number of images
+    num_samples = 0
+
+
+#image preprocessing
+    for dataset in data_dir_list:
+        img_list=os.listdir(data_path+'\\'+ dataset)
+        if img_list[0] == '.DS_Store':
+            img_list = os.listdir(data_path+'\\'+ dataset)[1:] 
+        num_samples +=len(img_list)
+        print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+        
+        for img in img_list:
+                input_img= imread(data_path + '\\'+ dataset + '\\'+ img, mode = img_mode)
+                
+                #input_img_grey=input_img.convert('L')
+                input_img_resize = imresize(input_img, (img_rows, img_cols))
+                img_data_list.append(input_img_resize)
+    
+    print("The total number of images: " + str(num_samples))
+        
+    print(len(img_data_list))
+    
+    #array of all the images
+    img_data = np.array(img_data_list, dtype = 'float32')
+    print(img_data.shape)
+    img_data = img_data.astype('float32')
+    img_data /= 255
+    print (img_data.shape)
+    
+    if num_channel==1:
+    	if K.image_dim_ordering()=='th':
+    		img_data= np.expand_dims(img_data, axis=1) 
+    		print (img_data.shape)
+    	else:
+    		img_data= np.expand_dims(img_data, axis=4) 
+    		print (img_data.shape)
+    		
+    else:
+    	if K.image_dim_ordering()=='th':
+    		img_data=np.rollaxis(img_data,3,1)
+    		print (img_data.shape)
+    
+   # create the array of labels
+    
+    label = np.ones((num_samples,), dtype=int)
+    count1 = 0
+    count2 = 0
+    for dirs in data_dir_list:
+        img_list=os.listdir(data_path+'\\'+ dirs)[1:]
+        count1, count2 =count2, count2 + len(img_list)
+        label[count1:count2] = dirs[1:3]
+        
+    #list of labels    
+    poi_list = ['neuschwanstein','cologne cathedral','brandenburger tor', 'heidelberg castle' , 
+                'marienplatz', 'frauenkirche dresden', 'berlin wall', 'reichstag', 
+                'nymphenburg', 'speicherstadt']
+    
+    
+    # convert class labels to on-hot encoding
+    Y = np_utils.to_categorical(label, num_classes)
+    
+    #Shuffle the dataset
+    x,y = shuffle(img_data,Y, random_state=2)
+    
+    # Split the dataset
+    X_train, X_test, y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=2)    
+    # Load Cifar10 data. Please implement your own load_data() module for your own dataset
+    #X_train, Y_train, X_valid, Y_valid = load_cifar10_data(img_rows, img_cols)
+
+#%%
+#
+#for i in range(0, 9):
+#    plt.subplot(330 + 1 + i)
+#    plt.imshow(toimage(X_train[i]))
+## show the plot
+#plt.show()
+
+#%%
+        # Load our model
+    model = resnet50_model(img_rows, img_cols, num_channel, num_classes)
+
+    # Start Fine-tuning
+    model.fit(X_train, y_train,
+              batch_size=batch_size,
+              epochs=nb_epoch,
+              shuffle=True,
+              verbose=1,
+              validation_data=(X_test, Y_test),
+              )
+
+    # Make predictions
+    predictions_valid = model.predict(X_test, batch_size=batch_size, verbose=1)
+
+    # Cross-entropy loss score
+    score = log_loss(Y_test, predictions_valid)
+    print(score)
+    
+#%%
+
+# Evaluating the model
+
+score = model.evaluate(X_test, Y_test, verbose=1)
+print('Test Loss:', score[0])
+print('Test accuracy:', score[1])
+
+#%%
+#Testing random image from test dataset
+test_image = X_test[229:230]
+
+print(model.predict(test_image))
+#print(model.predict_classes(test_image))
+print(Y_test[229:230])
+
+print((model.predict(test_image)))			
+pred = model.predict(test_image).reshape(10,)			
+print(pred)			
+			
+			
+max_val = np.amax(pred)			
+max_ind = np.argmax(pred)			
+			
+print('Test image belongs to: class 0' + str(max_ind) + ' "' + str(poi_list[max_ind]) + '" with the acccuracy: ' + str(max_val) )			
+
+img = test_image.reshape(img_rows, img_cols, num_channel)
+plt.imshow(img)
+#plt.imshow(img, cmap='gray')
+
+
+#%%
+# Testing a new image
+
+#test_image = imread(r'C:\Users\Administrator\Desktop\az\poi_dataset_200\train\c3_heidelberg castle\8001684059_821234e08f_c.jpg', mode = img_mode)
+
+
+#Testing the image from internet
+from skimage import io
+url = 'http://res.muenchen-p.de/fl_progressive,q_65/.imaging/stk/responsive/teaser300/dms/va-2016/oktoberfest-2016/anstich-2016/anstich-2016-reiter-hp/document/anstich-2016-reiter-hp.jpg'
+test_image = io.imread(url)
+
+
+#test_image=cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+test_image = imresize(test_image,(img_rows,img_cols))
+test_image = np.array(test_image)
+test_image = test_image.astype('float32')
+test_image /= 255
+#print (test_image.shape)
+   
+if num_channel==1:
+	if K.image_dim_ordering()=='th':
+		test_image= np.expand_dims(test_image, axis=0)
+		test_image= np.expand_dims(test_image, axis=0)
+		#print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=3) 
+		test_image= np.expand_dims(test_image, axis=0)
+		#print (test_image.shape)
+		
+else:
+	if K.image_dim_ordering()=='th':
+		test_image=np.rollaxis(test_image,2,0)
+		test_image= np.expand_dims(test_image, axis=0)
+		#print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=0)
+		#print (test_image.shape)
+		
+#print((model.predict(test_image)))			
+pred = model.predict(test_image).reshape(12,)			
+#print(pred)			
+			
+			
+max_val = np.amax(pred)			
+max_ind = np.argmax(pred)			
+			
+print('Test image belongs to: class 0' + str(max_ind) + ' "' + str(poi_list[max_ind][4:]) + '" with the acccuracy: ' + str(max_val) )		
+
+#%%
+
+# Visualizing the intermediate layer
+
+#
+def get_featuremaps(model, layer_idx, X_batch):
+	get_activations = K.function([model.layers[0].input, K.learning_phase()],[model.layers[layer_idx].output,])
+	activations = get_activations([X_batch,0])
+	return activations
+
+layer_num=3
+filter_num=0
+
+activations = get_featuremaps(model, int(layer_num),test_image)
+
+print (np.shape(activations))
+feature_maps = activations[0][0]      
+print (np.shape(feature_maps))
+
+if K.image_dim_ordering()=='th':
+	feature_maps=np.rollaxis((np.rollaxis(feature_maps,2,0)),2,0)
+print (feature_maps.shape)
+
+fig=plt.figure(figsize=(16,16))
+plt.imshow(feature_maps[:,:,filter_num],cmap='gray')
+plt.savefig("featuremaps-layer-{}".format(layer_num) + "-filternum-{}".format(filter_num)+'.jpg')
+
+num_of_featuremaps=feature_maps.shape[2]
+fig=plt.figure(figsize=(16,16))	
+plt.title("featuremaps-layer-{}".format(layer_num))
+subplot_num=int(np.ceil(np.sqrt(num_of_featuremaps)))
+for i in range(int(num_of_featuremaps)):
+	ax = fig.add_subplot(subplot_num, subplot_num, i+1)
+	#ax.imshow(output_image[0,:,:,i],interpolation='nearest' ) #to see the first filter
+	ax.imshow(feature_maps[:,:,i],cmap='gray')
+	plt.xticks([])
+	plt.yticks([])
+	plt.tight_layout()
+plt.show()
+fig.savefig("featuremaps-layer-{}".format(layer_num) + '.jpg')
+
+#%%
+# Printing the confusion matrix
+from sklearn.metrics import classification_report,confusion_matrix
+import itertools
+
+Y_pred = model.predict(X_test)
+print(Y_pred)
+y_pred = np.argmax(Y_pred, axis=1)
+print(y_pred)
+#y_pred = model.predict_classes(X_test)
+#print(y_pred)
+target_names = ['class 0(neuschwanstein)', 'class 1(cologne cathedral)', 'class 2(brandenburger tor)','class 3(heidelberg castle)', 
+                'class 4(marienplatz)', 'class 5(frauenkirche dresden)', 'class 6(berlin wall)', 'class 7(reichstag)',
+                'class 8(nymphenburg)', 'class 9(speicherstadt)', 'class 10(oktoberfest)', 'class 11(christkindlmarkt)' ]
+
+					
+print(classification_report(np.argmax(Y_test,axis=1), y_pred,target_names=target_names))
+
+print(confusion_matrix(np.argmax(Y_test,axis=1), y_pred))
+
+
+# Plotting the confusion matrix
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Compute confusion matrix
+cnf_matrix = (confusion_matrix(np.argmax(Y_test,axis=1), y_pred))
+
+np.set_printoptions(precision=2)
+
+plt.figure()
+
+# Plot non-normalized confusion matrix
+plot_confusion_matrix(cnf_matrix, classes=target_names,
+                      title='Confusion matrix')
+#plt.figure()
+# Plot normalized confusion matrix
+#plot_confusion_matrix(cnf_matrix, classes=target_names, normalize=True,
+#                      title='Normalized confusion matrix')
+#plt.figure()
+plt.show()
+
+#%%
+# Saving and loading model and weights
+from keras.models import model_from_json
+from keras.models import load_model
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("resnet_model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("resnet_model.h5")
+print("Saved model to disk")
+
+# load json and create model
+json_file = open('resnet_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("resnet_model.h5")
+print("Loaded model from disk")
+
+model.save('resnet_model.hdf5')
+loaded_model=load_model('resnet_model.hdf5')
+
+
+
